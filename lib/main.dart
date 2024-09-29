7b88eed2fe97ec5906ee8df0e64e1897b1089684
CEbbinghaus <git@cebbinghaus.com>
2024-09-30 03:51:56 +1000

    Changed main title

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Linkd',
       theme: ThemeData(
         // This is the theme of your application.
         //
