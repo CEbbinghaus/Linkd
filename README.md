8c911eba2b132c7c6c622a1a91218260f94da1bf
CEbbinghaus <git@cebbinghaus.com>
2024-09-30 03:44:09 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# linkd
+
+An app to manage which links get opened in which app
---
+++ android/app/src/main/kotlin/com/example/hello_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cebbinghaus.linkd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
